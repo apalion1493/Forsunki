431541a246157678f406a173a3e3c1da94de7d6a
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-12-21 17:58:05 +0200

    commit 21.12

--- app/js/pages/main.js
+++ app/js/pages/main.js
@@ -90,3 +90,38 @@ $('.list-logos__hidden-link').click(function () {
         $(this).siblings('.list-logos__hidden-block').addClass('active');
     }
 });
+
+//animation scroll
+$(function(){
+    $('.main-menu__link[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+
+        $('html, body').animate({scrollTop: dn}, 500);
+    });
+});
+
+
+
+//
+$('.container-burger').click(function () {
+    $(this).toggleClass('active');
+    $('.main-menu').toggleClass('active');
+});
+
+$('.main-menu__close').click(function () {
+    $('.container-burger').removeClass('active');
+    $('.main-menu').removeClass('active');
+});
+
+jQuery(function($){
+    $(document).mouseup(function (e){
+        let divMenu = $(".main-menu");
+        if (!divMenu.is(e.target) && divMenu.has(e.target).length === 0) {
+            $('.container-burger').removeClass('active');
+            divMenu.removeClass('active');
+        }
+    });
+});
